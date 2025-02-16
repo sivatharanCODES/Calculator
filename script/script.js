9d756050be6a61112cde22ff9017db6da1f61ba6
sivatharan <sivatharanoff97@gmail.com>
2025-02-16 20:53:39 +0530

    toggle effect added

--- script/script.js
+++ script/script.js
@@ -5,4 +5,9 @@ document.querySelector(".toggle").addEventListener("click", function () {
   //   document.querySelector(".toggle").style.justifyContent = "flex-start";
   //   document.querySelector(".container").style.backgroundColor = "black";
   console.log("toggle clicked");
+
+  document.querySelector(".fas").classList.toggle("fa-sun");
+  document.querySelector(".fas").classList.toggle("fa-moon");
+  document.querySelector(".fas").classList.toggle("toggled-icon");
+  //   document.querySelector(".fas").style.color = "rgb(255, 0, 170)";
 });
